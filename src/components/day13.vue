c08b108befd7f3dd161c12e005a0b141ecf72264
funnypan <gemini0525@foxmail.com>
2020-01-05 15:19:30 +0800

    add sentry

--- src/components/day13.vue
+++ src/components/day13.vue
@@ -1,13 +1,11 @@
 <template>
-    <div></div>
+	<div></div>
 </template>
 
 <script>
 export default {
-    name:"day13"
-}
+	name: "day13"
+};
 </script>
 
-<style lang="less">
-    
-</style>
+<style lang="less"></style>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,16 @@ import router from "./router";
 import "./registerServiceWorker";
 import "@/icons";
 import "@/assets/global.less";
+import * as Sentry from "@sentry/browser";
+import * as Integrations from "@sentry/integrations";
+
+Sentry.init({
+	dsn: "https://15439da9f58d40c1b0884afa4f51e16b@sentry.io/1871899",
+	integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+});
 
 Vue.config.productionTip = false;
-Vue.config.devtools = true;
+
 new Vue({
 	router,
 	render: h => h(App)
