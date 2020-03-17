715c02aba534854ddaa73bca720344dbabf5041f
funnypan <gemini0525@foxmail.com>
2020-03-17 18:15:07 +0800

    test actions

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 Vue.use(Router);
 
 export default new Router({
+	mode: process.env.NODE_ENV === "production" ? "history" : "hash",
 	routes: [
 		{
 			path: "/:id",
